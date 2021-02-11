044fd9f5d848dae09dd4b91e95f2cc3fe9bdd7f1
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-02-11 17:49:56 +0100

    add two methods to UserRepository

--- backend/src/main/java/xyz/stasiak/cobudgetbackend/users/UserRepository.java
+++ backend/src/main/java/xyz/stasiak/cobudgetbackend/users/UserRepository.java
@@ -1,5 +1,10 @@
 package xyz.stasiak.cobudgetbackend.users;
 
+import java.util.Optional;
+
 public interface UserRepository {
 
+    Optional<User> findById(String id);
+
+    Optional<User> findByUsername(String username);
 }
