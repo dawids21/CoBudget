f124450bc2c2f0c49e7225db595753fdbcd6a8c2
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-02-12 08:28:36 +0100

    create Expense model

--- backend/src/main/java/xyz/stasiak/cobudgetbackend/expense/Expense.java
+++ backend/src/main/java/xyz/stasiak/cobudgetbackend/expense/Expense.java
@@ -1,5 +1,53 @@
 package xyz.stasiak.cobudgetbackend.expense;
 
+import org.bson.types.Decimal128;
+
 public class Expense {
 
+    private int day;
+    private Decimal128 amount;
+    private String category;
+    private String subcategory;
+
+    public Expense() {
+    }
+
+    public Expense(int day, Decimal128 amount, String category, String subcategory) {
+        this.day = day;
+        this.amount = amount;
+        this.category = category;
+        this.subcategory = subcategory;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public Decimal128 getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Decimal128 amount) {
+        this.amount = amount;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getSubcategory() {
+        return subcategory;
+    }
+
+    public void setSubcategory(String subcategory) {
+        this.subcategory = subcategory;
+    }
 }
