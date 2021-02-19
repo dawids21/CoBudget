36f127f9b3256448a6c91992c6e815a962a85e20
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-02-19 11:42:00 +0100

    implement UserDetailsService

--- backend/src/main/java/xyz/stasiak/cobudgetbackend/security/SecurityConfig.java
+++ backend/src/main/java/xyz/stasiak/cobudgetbackend/security/SecurityConfig.java
@@ -3,13 +3,14 @@ package xyz.stasiak.cobudgetbackend.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import xyz.stasiak.cobudgetbackend.users.ApplicationUserRepository;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public UserDetailsServiceImpl userDetailsService() {
-        return new UserDetailsServiceImpl();
+    public UserDetailsServiceImpl userDetailsService(ApplicationUserRepository userRepository) {
+        return new UserDetailsServiceImpl(userRepository);
     }
 
     @Bean
--- backend/src/main/java/xyz/stasiak/cobudgetbackend/security/UserDetailsServiceImpl.java
+++ backend/src/main/java/xyz/stasiak/cobudgetbackend/security/UserDetailsServiceImpl.java
@@ -1,14 +1,28 @@
 package xyz.stasiak.cobudgetbackend.security;
 
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import xyz.stasiak.cobudgetbackend.users.ApplicationUser;
+import xyz.stasiak.cobudgetbackend.users.ApplicationUserRepository;
 
 public class UserDetailsServiceImpl implements UserDetailsService {
 
+    private final ApplicationUserRepository userRepository;
+
+    public UserDetailsServiceImpl(ApplicationUserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        //TODO implement loadUserByUsername
-        return null;
+        ApplicationUser user = userRepository.findByEmail(s)
+                                             .orElseThrow(
+                                                      () -> new UsernameNotFoundException("User " + s + " not found"));
+        return User.builder()
+                   .username(user.getEmail())
+                   .password(user.getPassword())
+                   .build();
     }
 }
