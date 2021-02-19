8a6be3a8582e6aeb7fedc38c4cb6f873b614fe39
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-02-19 11:43:24 +0100

    add password encryption when user exists

--- backend/src/main/java/xyz/stasiak/cobudgetbackend/users/ApplicationUserController.java
+++ backend/src/main/java/xyz/stasiak/cobudgetbackend/users/ApplicationUserController.java
@@ -1,6 +1,7 @@
 package xyz.stasiak.cobudgetbackend.users;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,13 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApplicationUserController {
 
     private final ApplicationUserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public ApplicationUserController(ApplicationUserRepository userRepository) {
+    public ApplicationUserController(ApplicationUserRepository userRepository,
+                                     BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     @PostMapping("/sign-up")
     public ResponseEntity<ApplicationUser> createUser(@RequestBody ApplicationUser applicationUser) {
+        applicationUser.setPassword(bCryptPasswordEncoder.encode(applicationUser.getPassword()));
         ApplicationUser user = userRepository.save(applicationUser);
         return ResponseEntity.ok(applicationUser);
         //TODO handle when email already exists
