3d5d8ffe97627a0b4dae8d5cd04432096cfc2693
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-02-22 13:37:32 +0100

    create class for ApplicationUserController tests

---
+++ backend/src/test/java/xyz/stasiak/cobudgetbackend/users/ApplicationUserControllerIT.java
@@ -0,0 +1,26 @@
+package xyz.stasiak.cobudgetbackend.users;
+
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@WebMvcTest(ApplicationUserController.class)
+class ApplicationUserControllerIT {
+
+    @MockBean
+    private ApplicationUserRepository repository;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        RestAssuredMockMvc.mockMvc(mockMvc);
+    }
+}
