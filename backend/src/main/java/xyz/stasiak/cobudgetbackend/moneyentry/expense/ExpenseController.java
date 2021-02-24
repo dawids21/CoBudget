40b9cff081fb5b5614dbaddb31cc2a3e44ed7010
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-02-24 12:23:55 +0100

    write basic method to handle adding expense in controller

--- backend/src/main/java/xyz/stasiak/cobudgetbackend/moneyentry/expense/ExpenseController.java
+++ backend/src/main/java/xyz/stasiak/cobudgetbackend/moneyentry/expense/ExpenseController.java
@@ -1,10 +1,26 @@
 package xyz.stasiak.cobudgetbackend.moneyentry.expense;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @RestController
 @RequestMapping("/expense")
 public class ExpenseController {
-   
+
+    private final AddExpenseService addExpenseService;
+
+    public ExpenseController(AddExpenseService addExpenseService) {
+        this.addExpenseService = addExpenseService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Expense> add(@RequestBody Expense expense, Principal principal) {
+        Expense result = addExpenseService.add(expense, principal.getName());
+        return ResponseEntity.ok(result);
+    }
 }
