8ddf0341c5a373b26d66483572d73ee4013d8e44
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-02-25 08:37:18 +0100

    create file for integration tests for ExpenseController

---
+++ backend/src/test/java/xyz/stasiak/cobudgetbackend/moneyentry/expense/ExpenseControllerIT.java
@@ -0,0 +1,9 @@
+package xyz.stasiak.cobudgetbackend.moneyentry.expense;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class ExpenseControllerIT {
+
+}
