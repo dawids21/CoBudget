16e6b99aee39cebe86e6749e50c02aaf07f09225
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-02-27 13:11:31 +0100

    use ValidationErrors in handler

--- backend/src/main/java/xyz/stasiak/cobudgetbackend/validation/ValidationExceptionHandler.java
+++ backend/src/main/java/xyz/stasiak/cobudgetbackend/validation/ValidationExceptionHandler.java
@@ -6,22 +6,18 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @RestControllerAdvice(annotations = ValidationExceptionProcessing.class)
 public class ValidationExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ValidationErrors> handleValidationExceptions(MethodArgumentNotValidException ex) {
         var validationErrors = new ValidationErrors();
-        Map<String, String> errors = new HashMap<>();
         ex.getBindingResult()
           .getAllErrors()
           .forEach((error) -> {
               String fieldName = ((FieldError) error).getField();
               String errorMessage = error.getDefaultMessage();
-              errors.put(fieldName, errorMessage);
+              validationErrors.addError(fieldName, errorMessage);
           });
         return ResponseEntity.badRequest()
                              .body(validationErrors);
