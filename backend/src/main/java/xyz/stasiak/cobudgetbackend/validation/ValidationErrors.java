800321b377d2ea18124f2f4ff252eee2298fe464
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-02-27 13:09:57 +0100

    add class to hold errors

--- backend/src/main/java/xyz/stasiak/cobudgetbackend/validation/ValidationErrors.java
+++ backend/src/main/java/xyz/stasiak/cobudgetbackend/validation/ValidationErrors.java
@@ -1,5 +1,21 @@
 package xyz.stasiak.cobudgetbackend.validation;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ValidationErrors {
 
+    private final Map<String, String> errors;
+
+    public ValidationErrors() {
+        this.errors = new HashMap<>();
+    }
+
+    public Map<String, String> getErrors() {
+        return errors;
+    }
+
+    public void addError(String fieldName, String message) {
+        errors.put(fieldName, message);
+    }
 }
