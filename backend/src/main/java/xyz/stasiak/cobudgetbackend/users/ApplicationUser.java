a294d767c461ba129e321738d2b23ae0ba0639b9
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-02-28 22:16:06 +0100

    add UserConfiguration field to ApplicationUser

--- backend/src/main/java/xyz/stasiak/cobudgetbackend/users/ApplicationUser.java
+++ backend/src/main/java/xyz/stasiak/cobudgetbackend/users/ApplicationUser.java
@@ -3,6 +3,7 @@ package xyz.stasiak.cobudgetbackend.users;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import xyz.stasiak.cobudgetbackend.users.config.UserConfiguration;
 
 import javax.validation.constraints.Email;
 
@@ -18,6 +19,7 @@ public class ApplicationUser {
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
     private String name;
+    private UserConfiguration userConfiguration = UserConfiguration.defaultConfiguration();
 
     //TODO make constructor package-private
     //TODO write method for constructing user object with default config
@@ -29,6 +31,13 @@ public class ApplicationUser {
         this.email = email;
         this.password = password;
         this.name = name;
+    }
+
+    public ApplicationUser(String email, String password, String name, UserConfiguration userConfiguration) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.userConfiguration = userConfiguration;
     }
 
     public String getId() {
@@ -61,5 +70,13 @@ public class ApplicationUser {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public UserConfiguration getUserConfiguration() {
+        return userConfiguration;
+    }
+
+    public void setUserConfiguration(UserConfiguration userConfiguration) {
+        this.userConfiguration = userConfiguration;
     }
 }
