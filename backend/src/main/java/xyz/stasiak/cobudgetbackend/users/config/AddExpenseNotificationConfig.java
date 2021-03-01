949a23061abe113e75e5b0153d83a389348d20d5
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-03-01 13:47:23 +0100

    write test to check if default config is used for new users

--- backend/src/main/java/xyz/stasiak/cobudgetbackend/users/config/AddExpenseNotificationConfig.java
+++ backend/src/main/java/xyz/stasiak/cobudgetbackend/users/config/AddExpenseNotificationConfig.java
@@ -1,5 +1,7 @@
 package xyz.stasiak.cobudgetbackend.users.config;
 
+import java.util.Objects;
+
 public class AddExpenseNotificationConfig {
 
     private boolean enabled;
@@ -17,5 +19,22 @@ public class AddExpenseNotificationConfig {
 
     public void setEnabled(boolean enabled) {
         this.enabled = enabled;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        AddExpenseNotificationConfig that = (AddExpenseNotificationConfig) o;
+        return isEnabled() == that.isEnabled();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(isEnabled());
     }
 }
--- backend/src/main/java/xyz/stasiak/cobudgetbackend/users/config/UserConfiguration.java
+++ backend/src/main/java/xyz/stasiak/cobudgetbackend/users/config/UserConfiguration.java
@@ -1,5 +1,7 @@
 package xyz.stasiak.cobudgetbackend.users.config;
 
+import java.util.Objects;
+
 public class UserConfiguration {
 
     private final AddExpenseNotificationConfig addExpenseNotificationConfig = new AddExpenseNotificationConfig();
@@ -19,5 +21,22 @@ public class UserConfiguration {
 
     public void toggleAddExpenseNotification() {
         addExpenseNotificationConfig.setEnabled(!addExpenseNotificationConfig.isEnabled());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        UserConfiguration that = (UserConfiguration) o;
+        return Objects.equals(addExpenseNotificationConfig, that.addExpenseNotificationConfig);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(addExpenseNotificationConfig);
     }
 }
