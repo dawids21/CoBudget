1de2691c7e1b8ce2a778eda87bb79ee2139954de
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-03-01 15:07:25 +0100

    add mapping to get user configuration

--- backend/src/main/java/xyz/stasiak/cobudgetbackend/users/ApplicationUserController.java
+++ backend/src/main/java/xyz/stasiak/cobudgetbackend/users/ApplicationUserController.java
@@ -3,8 +3,10 @@ package xyz.stasiak.cobudgetbackend.users;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+import xyz.stasiak.cobudgetbackend.users.config.UserConfiguration;
 
 import javax.validation.Valid;
+import java.security.Principal;
 
 @RestController
 @RequestMapping("/user")
@@ -31,5 +33,16 @@ public class ApplicationUserController {
         ApplicationUser user = userRepository.save(applicationUser.toApplicationUser(bCryptPasswordEncoder));
         return ResponseEntity.ok(new ApplicationUserReadModel(user));
         //TODO handle when email already exists
+    }
+
+    @GetMapping("/config")
+    public ResponseEntity<UserConfiguration> getUserConfiguration(Principal principal) {
+        var user = userRepository.findByEmail(principal.getName());
+        if (user.isEmpty()) {
+            return ResponseEntity.notFound()
+                                 .build();
+        }
+        return ResponseEntity.ok(user.get()
+                                     .getUserConfiguration());
     }
 }
