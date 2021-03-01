bed3a864bbe0033b88f92732d341e568463c2b76
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-03-01 17:53:04 +0100

    create js file for login

---
+++ frontend/js/login.js
@@ -0,0 +1,38 @@
+import FetchService from "./service/FetchService.js";
+
+const fetchService = new FetchService();
+
+async function submitForm(e, form) {
+    e.preventDefault();
+    const btnSubmit = document.getElementById('sign-in-submit');
+    btnSubmit.disabled = true;
+    setTimeout(() => btnSubmit.disabled = false, 2000);
+    const jsonFormData = buildJsonFormData(form);
+    const headers = buildHeaders();
+    try {
+        const response = await fetchService.performPostHttpRequest('http://localhost:8080/user/sign-up', headers, jsonFormData);
+    } catch (e) {
+        alert("Cannot perform login. Please try again");
+        return;
+    }
+    window.location.href = "/monthView.html";
+}
+
+function buildJsonFormData(form) {
+    const jsonFormData = {};
+    for (const pair of new FormData(form)) {
+        jsonFormData[pair[0]] = pair[1];
+    }
+    return jsonFormData;
+}
+
+function buildHeaders() {
+    return {
+        "Content-Type": "application/json",
+    };
+}
+
+const form = document.getElementById('login-form');
+form.addEventListener('submit', function (e) {
+    submitForm(e, this);
+});
