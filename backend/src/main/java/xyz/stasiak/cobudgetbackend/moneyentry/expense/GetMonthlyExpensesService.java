ceeaf97d9765b8db677c443cfe2c1849e7262992
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-03-03 13:19:33 +0100

    write first test for service

--- backend/src/main/java/xyz/stasiak/cobudgetbackend/moneyentry/expense/GetMonthlyExpensesService.java
+++ backend/src/main/java/xyz/stasiak/cobudgetbackend/moneyentry/expense/GetMonthlyExpensesService.java
@@ -1,5 +1,11 @@
 package xyz.stasiak.cobudgetbackend.moneyentry.expense;
 
+import xyz.stasiak.cobudgetbackend.date.MonthAndYearDate;
+
 public class GetMonthlyExpensesService {
 
+    public MonthlyExpenses getExpenses(String username, MonthAndYearDate date) {
+        //TODO implement getExpenses
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
 }
--- backend/src/test/java/xyz/stasiak/cobudgetbackend/moneyentry/expense/GetMonthlyExpensesServiceTest.java
+++ backend/src/test/java/xyz/stasiak/cobudgetbackend/moneyentry/expense/GetMonthlyExpensesServiceTest.java
@@ -1,16 +1,28 @@
 package xyz.stasiak.cobudgetbackend.moneyentry.expense;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class GetMonthlyExpensesServiceTest {
 
+    private GetMonthlyExpensesService service;
+
+    @BeforeEach
+    void setUp() {
+        service = new GetMonthlyExpensesService();
+    }
+
     @Test
     void get_monthly_expenses_for_given_user_and_date() {
-        //TODO implement get_monthly_expenses_for_given_user_and_date
-        throw new UnsupportedOperationException("Not implemented yet");
+        MonthlyExpenses expenses = service.getExpenses(TestExpenseConfig.TEST_USERNAME, TestExpenseConfig.CURRENT_DATE);
+        assertThat(expenses.getMonth()).isEqualTo(TestExpenseConfig.CURRENT_DATE.getMonth());
+        assertThat(expenses.getYear()).isEqualTo(TestExpenseConfig.CURRENT_DATE.getYear());
+        assertThat(expenses.getUsername()).isEqualTo(TestExpenseConfig.TEST_USERNAME);
     }
 
     @Test
