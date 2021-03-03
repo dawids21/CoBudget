f6dbafd8adafb84130b5a07eb0a0e6341828b4a5
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-03-03 14:13:56 +0100

    add ResponseStatus to bad request exception

--- backend/src/main/java/xyz/stasiak/cobudgetbackend/moneyentry/EntryBadRequestException.java
+++ backend/src/main/java/xyz/stasiak/cobudgetbackend/moneyentry/EntryBadRequestException.java
@@ -1,5 +1,9 @@
 package xyz.stasiak.cobudgetbackend.moneyentry;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class EntryBadRequestException extends EntryException {
 
     public EntryBadRequestException(String message) {
