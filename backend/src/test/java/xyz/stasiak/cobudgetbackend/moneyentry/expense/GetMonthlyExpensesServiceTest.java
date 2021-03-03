2bd97fe4acebc7b7b079df03e348ff9bb19aee3b
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-03-03 13:15:24 +0100

    create test methods for service

--- backend/src/test/java/xyz/stasiak/cobudgetbackend/moneyentry/expense/GetMonthlyExpensesServiceTest.java
+++ backend/src/test/java/xyz/stasiak/cobudgetbackend/moneyentry/expense/GetMonthlyExpensesServiceTest.java
@@ -2,8 +2,20 @@ package xyz.stasiak.cobudgetbackend.moneyentry.expense;
 
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class GetMonthlyExpensesServiceTest {
 
+    @Test
+    void get_monthly_expenses_for_given_user_and_date() {
+        //TODO implement get_monthly_expenses_for_given_user_and_date
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    @Test
+    void throw_exception_when_expenses_not_found() {
+        //TODO implement throw_exception_when_expenses_not_found
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
 }
