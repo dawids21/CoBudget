fa4face44f3e458a6caa32964b885269514c6b72
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-03-03 13:50:52 +0100

    add exception for EntryNotFound

---
+++ backend/src/main/java/xyz/stasiak/cobudgetbackend/moneyentry/EntryNotFound.java
@@ -0,0 +1,9 @@
+package xyz.stasiak.cobudgetbackend.moneyentry;
+
+
+public class EntryNotFound extends EntryException {
+
+    public EntryNotFound(String message) {
+        super(message);
+    }
+}
