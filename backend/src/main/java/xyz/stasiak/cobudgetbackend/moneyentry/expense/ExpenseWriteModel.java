33056d89a7e668d146f97293cc9a107b1c539df5
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-03-11 12:49:36 +0100

    use string date in ExpenseWriteModel

--- backend/src/main/java/xyz/stasiak/cobudgetbackend/moneyentry/expense/ExpenseWriteModel.java
+++ backend/src/main/java/xyz/stasiak/cobudgetbackend/moneyentry/expense/ExpenseWriteModel.java
@@ -1,24 +1,27 @@
 package xyz.stasiak.cobudgetbackend.moneyentry.expense;
 
 import xyz.stasiak.cobudgetbackend.date.MonthAndYearDate;
+import xyz.stasiak.cobudgetbackend.validation.CheckDateFormat;
 
 import javax.validation.Valid;
+import java.time.LocalDate;
 
 public class ExpenseWriteModel {
 
-    @Valid
-    private final MonthAndYearDate date;
+    @CheckDateFormat(pattern = "uuuu-MM-dd", message = "Date is invalid")
+    private final String date;
 
     @Valid
     private final Expense expense;
 
-    public ExpenseWriteModel(MonthAndYearDate date, Expense expense) {
+    public ExpenseWriteModel(String date, Expense expense) {
         this.date = date;
         this.expense = expense;
     }
 
     public MonthAndYearDate getDate() {
-        return date;
+        var localDate = LocalDate.parse(date);
+        return new MonthAndYearDate(localDate.getMonth(), localDate.getYear());
     }
 
     public Expense getExpense() {
