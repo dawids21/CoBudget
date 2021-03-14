4deb4095fad5bc013b3b7fd92b5d79422eea714f
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-03-14 16:26:39 +0100

    change get request for expenses from json to query params

--- backend/src/main/java/xyz/stasiak/cobudgetbackend/moneyentry/expense/ExpenseController.java
+++ backend/src/main/java/xyz/stasiak/cobudgetbackend/moneyentry/expense/ExpenseController.java
@@ -1,16 +1,22 @@
 package xyz.stasiak.cobudgetbackend.moneyentry.expense;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import xyz.stasiak.cobudgetbackend.date.MonthAndYearDate;
 import xyz.stasiak.cobudgetbackend.validation.ValidationExceptionProcessing;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Positive;
 import java.security.Principal;
+import java.time.Month;
 
 @RestController
 @RequestMapping("/expense")
 @ValidationExceptionProcessing
+@Validated
 public class ExpenseController {
 
     private final AddExpenseService addExpenseService;
@@ -28,7 +34,10 @@ public class ExpenseController {
     }
 
     @GetMapping
-    public ResponseEntity<MonthlyExpenses> getExpenses(@Valid @RequestBody MonthAndYearDate date, Principal principal) {
+    public ResponseEntity<MonthlyExpenses> getExpenses(@RequestParam("month") @Min(1) @Max(12) int month,
+                                                       @RequestParam("year") @Positive(message = "year must be greater than 0") int year,
+                                                       Principal principal) {
+        var date = new MonthAndYearDate(Month.of(month), year);
         return ResponseEntity.ok(getMonthlyExpensesService.getExpenses(principal.getName(), date));
     }
 }
