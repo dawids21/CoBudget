da068b9a68250bb0232c4570a83975e0810149fe
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-03-14 11:50:30 +0100

    create method for getting monthly expenses in service

--- frontend/js/service/GetExpensesService.js
+++ frontend/js/service/GetExpensesService.js
@@ -2,4 +2,8 @@ export default class GetExpensesService {
     constructor(requestService) {
         this.requestService = requestService;
     }
+
+    getMontlhlyExpenses(month, year) {
+        return this.requestService.getMontlhlyExpenses(month, year);
+    }
 }
