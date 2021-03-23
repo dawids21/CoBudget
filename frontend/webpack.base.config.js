1403fb0e447aa8dcb180e3177ce8f74a50ef7b83
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-03-23 23:34:26 +0100

    change name of script for week page

--- frontend/webpack.base.config.js
+++ frontend/webpack.base.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: {
         login: './src/js/login.js',
-        week: './src/js/WeekView.js',
+        week: './src/js/week.js',
     },
     module: {
         rules: [
