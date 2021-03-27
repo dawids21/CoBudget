cd0076e26098124749949eae7c0060390915d29c
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-03-27 14:37:35 +0100

    add favicon plugin to webpack config

--- frontend/webpack.base.config.js
+++ frontend/webpack.base.config.js
@@ -1,3 +1,5 @@
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
+
 module.exports = {
     entry: {
         login: './src/js/login.js',
@@ -6,6 +8,21 @@ module.exports = {
         index: './src/js/index.js',
         addExpense: './src/js/addExpense.js',
     },
+    plugins: [
+        new FaviconsWebpackPlugin({
+            logo: './src/icon.png',
+            cache: true,
+            prefix: 'icons-[contenthash:8]/',
+            favicons: {
+                appName: 'CoBudget',
+                appShortName: 'CoBudget',
+                background_color: '#E9ECEC',
+                theme_color: '#478401',
+                orientation: 'portrait-primary',
+                start_url: '/index.html',
+            },
+        }),
+    ],
     module: {
         rules: [
             {
