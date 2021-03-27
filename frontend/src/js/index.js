c8ab24e337d6f7e9a6ac640311c55a7916c5c101
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-03-27 18:07:16 +0100

    add service worker register to all files

--- frontend/src/js/index.js
+++ frontend/src/js/index.js
@@ -7,11 +7,11 @@ if (!jwtService.checkExpire()) {
     window.location.href = '/week.html';
 }
 
-//if ("serviceWorker" in navigator) {
-//    window.addEventListener("load", function () {
-//        navigator.serviceWorker
-//            .register("/serviceWorker.js")
-//            .then(res => console.log("service worker registered"))
-//            .catch(err => console.log("service worker not registered", err));
-//    });
-//}
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
+        navigator.serviceWorker
+            .register('/service-worker.js')
+            .then(res => console.log('service worker registered'))
+            .catch(err => console.log('service worker not registered', err));
+    });
+}
