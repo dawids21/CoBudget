96490f6ea87fd347aa71231893832cdec2324b5b
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-03-30 22:37:56 +0200

    add security properties to test security config

--- backend/src/test/java/xyz/stasiak/cobudgetbackend/security/jwt/SecurityTestConfig.java
+++ backend/src/test/java/xyz/stasiak/cobudgetbackend/security/jwt/SecurityTestConfig.java
@@ -1,5 +1,27 @@
 package xyz.stasiak.cobudgetbackend.security.jwt;
 
+import xyz.stasiak.cobudgetbackend.security.SecurityProperties;
+
 class SecurityTestConfig {
 
+    private final SecurityProperties securityProperties;
+
+    public SecurityTestConfig() {
+        this.securityProperties = testSecurityProperties();
+    }
+
+    private SecurityProperties testSecurityProperties() {
+        var securityProperties = new SecurityProperties();
+        var jwtProperties = new SecurityProperties.Jwt();
+        jwtProperties.setSecret("secret");
+        jwtProperties.setAccessTokenCookieName("accessCookie");
+        jwtProperties.setRefreshTokenCookieName("refreshCookie");
+        jwtProperties.setHeaderString("Authorization");
+        jwtProperties.setTokenPrefix("Bearer ");
+        jwtProperties.setAccessTokenExpirationDate(3600000L);
+        jwtProperties.setRefreshTokenExpirationDate(7776000000L);
+        jwtProperties.setSignUpUrl("/user/sign-up");
+        securityProperties.setJwt(jwtProperties);
+        return securityProperties;
+    }
 }
