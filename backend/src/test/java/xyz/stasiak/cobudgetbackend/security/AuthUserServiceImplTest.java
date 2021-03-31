c925f6f0ce19b175871146b53456242b5565b9e0
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-03-31 10:12:05 +0200

    create file for tests for AuthUserService implementation

---
+++ backend/src/test/java/xyz/stasiak/cobudgetbackend/security/AuthUserServiceImplTest.java
@@ -0,0 +1,9 @@
+package xyz.stasiak.cobudgetbackend.security;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class AuthUserServiceImplTest {
+
+}
