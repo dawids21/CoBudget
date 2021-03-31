9052a92ebee45e3a69039f8edb2727489225fc74
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-03-31 09:10:10 +0200

    implement tests for token provider

--- backend/src/test/java/xyz/stasiak/cobudgetbackend/security/jwt/TokenProviderImplTest.java
+++ backend/src/test/java/xyz/stasiak/cobudgetbackend/security/jwt/TokenProviderImplTest.java
@@ -1,71 +1,86 @@
 package xyz.stasiak.cobudgetbackend.security.jwt;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
+import xyz.stasiak.cobudgetbackend.security.SecurityProperties;
 
 import java.time.Clock;
 import java.time.Instant;
 import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class TokenProviderImplTest {
 
-    private TokenProvider tokenProvider;
-    private Clock clock;
-
-    @BeforeEach
-    void setUp() {
-        clock = Clock.fixed(Instant.now(), ZoneId.systemDefault());
-        tokenProvider = new TokenProviderImpl(clock, new SecurityTestConfig().securityProperties);
-    }
+    private final Clock clock = Clock.fixed(Instant.now(), ZoneId.systemDefault());
+    private final SecurityProperties securityProperties = new SecurityTestConfig().securityProperties;
+    private final TokenProvider tokenProvider = new TokenProviderImpl(clock, securityProperties);
 
     @Test
     void set_expiry_date_for_access_token_based_on_properties() {
-        //TODO implement set_expiry_date_for_access_token_based_on_properties
-        throw new UnsupportedOperationException("Not implemented yet");
+        var token = testAccessToken();
+        assertThat(token.getExpiryDate()).isEqualTo(Instant.now(clock)
+                                                           .plusMillis(securityProperties.getJwt()
+                                                                                         .getAccessTokenExpirationDate()));
     }
+
 
     @Test
     void set_expiry_date_for_refresh_token_based_on_properties() {
-        //TODO implement set_expiry_date_for_access_token_based_on_properties
-        throw new UnsupportedOperationException("Not implemented yet");
+        var token = testRefreshToken();
+        assertThat(token.getExpiryDate()).isEqualTo(Instant.now(clock)
+                                                           .plusMillis(securityProperties.getJwt()
+                                                                                         .getRefreshTokenExpirationDate()));
     }
 
     @Test
     void access_token_should_have_correct_type() {
-        //TODO implement access_token_should_have_correct_type
-        throw new UnsupportedOperationException("Not implemented yet");
+        var token = testAccessToken();
+        assertThat(token.getTokenType()).isEqualTo(Token.TokenType.ACCESS);
     }
 
     @Test
     void refresh_token_should_have_correct_type() {
-        //TODO implement access_token_should_have_correct_type
-        throw new UnsupportedOperationException("Not implemented yet");
+        var token = testRefreshToken();
+        assertThat(token.getTokenType()).isEqualTo(Token.TokenType.REFRESH);
     }
 
     @Test
     void return_subject_of_the_token() {
-        //TODO implement return_subject_of_the_token
-        throw new UnsupportedOperationException("Not implemented yet");
+        var token = testAccessToken();
+        assertThat(tokenProvider.getUsernameFromToken(token.getTokenValue())).isEqualTo("abc");
     }
 
     @Test
     void return_expiry_date_of_the_token() {
-        //TODO implement return_expiry_date_of_the_token
-        throw new UnsupportedOperationException("Not implemented yet");
+        var token = testAccessToken();
+        var expiryDate = token.getExpiryDate();
+        assertThat(tokenProvider.getExpiryDateFromToken(token.getTokenValue())).isEqualTo(
+                 expiryDate.truncatedTo(ChronoUnit.SECONDS));
     }
 
     @Test
     void validate_correct_token() {
-        //TODO implement validate_correct_token
-        throw new UnsupportedOperationException("Not implemented yet");
+        var tokenValue = testAccessToken().getTokenValue();
+        assertThat(tokenProvider.validateToken(tokenValue)).isTrue();
     }
 
     @Test
     void return_false_for_token_that_is_invalid() {
-        //TODO implement return_false_for_token_that_is_invalid
-        throw new UnsupportedOperationException("Not implemented yet");
+        var tokenValue = testAccessToken().getTokenValue();
+        var changedTokenValue = tokenValue.concat("a");
+        assertThat(tokenProvider.validateToken(changedTokenValue)).isFalse();
     }
+
+    private Token testAccessToken() {
+        return tokenProvider.generateAccessToken("abc");
+    }
+
+    private Token testRefreshToken() {
+        return tokenProvider.generateRefreshToken("abc");
+    }
+
 }
