ff61ed009d3ad84f13a103faa50e5605d773a2b7
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-03-31 10:11:34 +0200

    create file for AuthUserService implementation

---
+++ backend/src/main/java/xyz/stasiak/cobudgetbackend/security/AuthUserServiceImpl.java
@@ -0,0 +1,14 @@
+package xyz.stasiak.cobudgetbackend.security;
+
+public class AuthUserServiceImpl implements AuthUserService {
+
+    @Override
+    public void login(String email, String password, String accessToken, String refreshToken) {
+        
+    }
+
+    @Override
+    public void refresh(String accessToken, String refreshToken) {
+
+    }
+}
