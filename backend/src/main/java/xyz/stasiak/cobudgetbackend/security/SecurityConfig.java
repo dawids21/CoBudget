bbaf2aeebb8f052b5ddcc51e0ed7c2f8472b5f16
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-04-01 09:03:30 +0200

    add some new beans to SecurityConfig

--- backend/src/main/java/xyz/stasiak/cobudgetbackend/security/SecurityConfig.java
+++ backend/src/main/java/xyz/stasiak/cobudgetbackend/security/SecurityConfig.java
@@ -3,7 +3,11 @@ package xyz.stasiak.cobudgetbackend.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import xyz.stasiak.cobudgetbackend.security.jwt.TokenProvider;
+import xyz.stasiak.cobudgetbackend.security.jwt.TokenProviderImpl;
 import xyz.stasiak.cobudgetbackend.users.ApplicationUserRepository;
+
+import java.time.Clock;
 
 @Configuration
 public class SecurityConfig {
@@ -16,5 +20,16 @@ public class SecurityConfig {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public TokenProvider tokenProvider(SecurityProperties securityProperties) {
+        return new TokenProviderImpl(Clock.systemUTC(), securityProperties);
+    }
+
+    @Bean
+    public AuthUserService authUserService(ApplicationUserRepository userRepository, TokenProvider tokenProvider,
+                                           SecurityProperties securityProperties) {
+        return new AuthUserServiceImpl(userRepository, tokenProvider, new TokenCookieUtil(securityProperties.getJwt()));
     }
 }
