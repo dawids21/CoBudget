c6aa5206280fc2d3e61d49dfe5acad78ae26262d
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-04-01 22:33:58 +0200

    add everything to web security config

--- backend/src/main/java/xyz/stasiak/cobudgetbackend/security/WebSecurity.java
+++ backend/src/main/java/xyz/stasiak/cobudgetbackend/security/WebSecurity.java
@@ -1,31 +1,37 @@
 package xyz.stasiak.cobudgetbackend.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import xyz.stasiak.cobudgetbackend.security.jwt.JWTAuthenticationFilter;
-import xyz.stasiak.cobudgetbackend.security.jwt.JWTAuthorizationFilter;
+import xyz.stasiak.cobudgetbackend.security.jwt.TokenAuthenticationFilter;
 
+@Configuration
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     private final UserDetailsServiceImpl userDetailsService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final SecurityProperties securityProperties;
+    private final TokenAuthenticationFilter tokenAuthenticationFilter;
 
     public WebSecurity(UserDetailsServiceImpl userService, BCryptPasswordEncoder bCryptPasswordEncoder,
-                       SecurityProperties securityProperties) {
+                       SecurityProperties securityProperties, TokenAuthenticationFilter tokenAuthenticationFilter) {
         this.userDetailsService = userService;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
         this.securityProperties = securityProperties;
+        this.tokenAuthenticationFilter = tokenAuthenticationFilter;
     }
 
     @Override
@@ -35,6 +41,8 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
             .csrf()
             .disable()
             .authorizeRequests()
+            .antMatchers(HttpMethod.POST, "/auth/**")
+            .permitAll()
             .antMatchers(HttpMethod.POST, securityProperties.getJwt()
                                                             .getSignUpUrl())
             .permitAll()
@@ -43,14 +51,18 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
             .anyRequest()
             .authenticated()
             .and()
-            .addFilter(new JWTAuthenticationFilter(authenticationManager(), securityProperties))
-            .addFilter(new JWTAuthorizationFilter(authenticationManager(), securityProperties))
-            // this disables session creation on Spring Security
             .sessionManagement()
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
             .exceptionHandling()
-            .authenticationEntryPoint(new RestAuthenticationEntryPoint());
+            .authenticationEntryPoint(new RestAuthenticationEntryPoint())
+            .and()
+            .formLogin()
+            .disable()
+            .httpBasic()
+            .disable();
+
+        http.addFilterBefore(tokenAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
@@ -67,5 +79,10 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         source.registerCorsConfiguration("/**", corsConfiguration);
 
         return source;
+    }
+
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
