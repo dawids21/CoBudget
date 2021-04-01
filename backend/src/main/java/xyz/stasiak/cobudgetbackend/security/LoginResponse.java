51ce631a53394a91c4016f6efd9a8390c102e30b
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-04-01 08:42:12 +0200

    create class for Login Response

--- backend/src/main/java/xyz/stasiak/cobudgetbackend/security/LoginResponse.java
+++ backend/src/main/java/xyz/stasiak/cobudgetbackend/security/LoginResponse.java
@@ -1,5 +1,49 @@
 package xyz.stasiak.cobudgetbackend.security;
 
+import java.util.Objects;
+
 public class LoginResponse {
+
+    private final SuccessFailure status;
+    private final String message;
+
+    public LoginResponse(SuccessFailure status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public SuccessFailure getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        LoginResponse that = (LoginResponse) o;
+        return getStatus() == that.getStatus() && Objects.equals(getMessage(), that.getMessage());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getStatus(), getMessage());
+    }
+
+    @Override
+    public String toString() {
+        return "LoginResponse{" + "status=" + status + ", message='" + message + '\'' + '}';
+    }
+
+    public enum SuccessFailure {
+        SUCCESS, FAILURE
+    }
 
 }
