f55ce1b460bccfa88526c8733c674eb4d6ac0c6c
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-04-16 13:01:35 +0200

    rename css module to LabeledInput

--- frontend/src/js/components/common/LabeledInput.js
+++ frontend/src/js/components/common/LabeledInput.js
@@ -1,11 +1,11 @@
-import styles from './LabeledInput.module.css';
+import styles from './common.module.css';
 import React from 'react';
 
 const LabeledInput = (props) => {
     return (
         <div className={styles.labeledInput}>
-            <label className={styles.label} htmlFor={props.inputName}>{props.label}</label>
-            <input className={styles.input} type={props.type} id={props.inputName} name={props.inputName}
+            <label htmlFor={props.inputName}>{props.label}</label>
+            <input type={props.type} id={props.inputName} name={props.inputName}
                    onChange={props.onChange}/>
         </div>
     );
