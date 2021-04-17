afa8821c84eb531b2b8d7659062a6df7ed4f3518
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-04-17 09:41:07 +0200

    use font awesome icon for react in form

--- frontend/src/js/components/common/Form.js
+++ frontend/src/js/components/common/Form.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from './Form.module.css';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faAngleLeft} from '@fortawesome/free-solid-svg-icons';
 
 const LogoContainer = () => {
     return (
@@ -13,8 +15,9 @@ const Form = (props) => {
     return (
         <div className={styles.container}>
             <LogoContainer/>
-            <a className={styles.formBackButton} href={props.returnPage}><i
-                className="fas fa-angle-left fa-3x"/></a>
+            <a className={styles.formBackButton} href={props.returnPage}>
+                <FontAwesomeIcon icon={faAngleLeft} size="3x"/>
+            </a>
             <form className={styles.form} onSubmit={(event) => props.onSubmit(event)}>
                 <div className={styles.formData}>
                     {props.children}
