bd5e89a80b4dff06b79c376ed164735f8fa1cc12
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-05-06 19:42:25 +0200

    add basic methods to repository

--- backend/src/main/java/xyz/stasiak/cobudgetbackend/entry/domain/CategoryRepository.java
+++ backend/src/main/java/xyz/stasiak/cobudgetbackend/entry/domain/CategoryRepository.java
@@ -2,4 +2,9 @@ package xyz.stasiak.cobudgetbackend.entry.domain;
 
 interface CategoryRepository {
 
+    Category save(Category toSave);
+
+    Category findAllByUsername(String username);
+
+    void deleteByUsername(String username);
 }
